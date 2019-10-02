c65c740e0ba0808839a8376784df9f3969bca700
Niels Saurer <me@nielssaurer.com>
2019-10-02 22:47:52 +0200

    Improve timeout error handling

--- main.go
+++ main.go
@@ -83,7 +83,7 @@ func handleRequest(conn net.Conn, redisClient *redis.Client) {
 	for {
 		n, err := conn.Read(buf)
 		if err != nil {
-			if err != io.EOF && !strings.Contains(err.Error(), "i/o timeout") {
+			if netErr, _ := err.(net.Error); err != io.EOF && !netErr.Timeout() {
 				fmt.Println("read error:", err)
 				conn.Write([]byte("read err\r\n"))
 				conn.Close()
